304ec18ce960b60d0d03a91ac2cab25e8bd4e811
fernando duenas <fernando39615@aim.com>
2020-11-12 10:24:16 -0500

    commit

---
+++ badDay.js
@@ -0,0 +1,16 @@
+const badDay = require('./day');
+
+
+const arr = [];
+const func = function(){
+    // console.log(arr);
+    // for(let i = 0; i < str.length; i++){
+        // arr.push(i)
+        console.log(`this is a ${badDay}, for everyone`);
+    }
+    // return arr;
+// }
+// func(badDay);
+// for(const badDay of badDays){
+    setInterval(func, 1000)
+// }                       
